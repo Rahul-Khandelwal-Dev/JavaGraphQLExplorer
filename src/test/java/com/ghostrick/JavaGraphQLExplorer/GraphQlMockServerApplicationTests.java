f46c5f45ff8478b0613c361217e2e3d126446182
Rahul Khandelwal <163019225+Rahul-Khandelwal-Dev@users.noreply.github.com>
2024-03-12 00:42:46 +0800

    #1: initial commit

---
+++ src/test/java/com/ghostrick/JavaGraphQLExplorer/GraphQlMockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostrick.JavaGraphQLExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlMockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
